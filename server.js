50e15012912d6eca6d068ea03625c169553a764f
K Sheenan <kaynesheenan+git@gmail.com>
2021-09-06 14:15:02 +0900

    using a variable called code to pass in code to our website.

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get("/", (req, res) => {
-    res.render("code-display", { code: 'This is the code'})
+    const code = `Welcome to WasteBin!
+    Use the commands in the top right corner to create a new file to share with others.`
+    res.render("code-display", { code })
 })
 
 app.listen(3000)
