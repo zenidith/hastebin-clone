c22d6d0fa3f7ab76319613c47ac1c23fd8e9cec2
K Sheenan <kaynesheenan+git@gmail.com>
2021-09-06 14:35:48 +0900

    added highlight.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ app.set('view engine', 'ejs')
 
 app.get("/", (req, res) => {
     const code = `Welcome to WasteBin!
-    Use the commands in the top right corner to create a new file to share with others.`
+
+    Use the commands in the top right corner
+    to create a new file to share with others.`
     res.render("code-display", { code })
 })
 
