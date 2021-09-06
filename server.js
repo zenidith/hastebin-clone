a57e9670a80acddefb7c25b81716c99fa2e388b9
K Sheenan <kaynesheenan+git@gmail.com>
2021-09-06 14:11:10 +0900

    created view folder and code-display.ejs, practicing with the ejs file to render code in the html

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs')
+
+app.get("/", (req, res) => {
+    res.render("code-display", { code: 'This is the code'})
+})
 
 app.listen(3000)
+ 
