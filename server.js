b46869263ec9398abea30dfeebafb3b9e0256db6
K Sheenan <kaynesheenan+git@gmail.com>
2021-09-09 13:39:29 +0900

    fixed the CSS to work OK!

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     const code = `Welcome to WasteBin!
 
-    Use the commands in the top right corner
-    to create a new file to share with others.`
+Use the commands in the top right corner
+to create a new file to share with others.`
     res.render("code-display", { code })
 })
 
