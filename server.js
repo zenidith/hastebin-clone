114733f17e74f62ee25550358657eae43067f9c1
K Sheenan <kaynesheenan+git@gmail.com>
2021-09-10 14:03:59 +0900

    added the _buttons.ejs file in order to style the buttons part of the new.js

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ app.set('view engine', 'ejs')
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    const code = `Welcome to WasteBin!
-
+const code = `Welcome to WasteBin!
 Use the commands in the top right corner
 to create a new file to share with others.`
     res.render("code-display", { code })
