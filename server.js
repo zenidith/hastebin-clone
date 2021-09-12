d7f849b5534979d8dd2f3f8b4ca237b7e87eeddc
K Sheenan <kaynesheenan+git@gmail.com>
2021-09-12 23:55:24 +0900

    adding mongodb

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+
+const mongoose = require('mongoose')
+mongoose.connect("monogodb://localhost/wastebin", {})
 
 app.get("/", (req, res) => {
 const code = `Welcome to WasteBin!
@@ -13,6 +17,11 @@ to create a new file to share with others.`
 app.get('/new', (req, res) => {
     res.render("new")
 })
+
+app.post("/save", async (req, res) => {
+    const value = req.body.value
+    console.log(value)
+}
 
 app.listen(3000)
  
